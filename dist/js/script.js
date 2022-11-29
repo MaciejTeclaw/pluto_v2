dba0c9ec42ce96b9999098c777107ccd156cf55b
maciejteclaw1990 <maciejteclaw1990@gmail.com>
2022-11-29 13:34:15 +0100

    create navbar and simple animation

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,31 @@
-// $(function () {
-//   //show and hide search bar in header
-//   $(".site-header .search-icon").click(function () {
-//     $(".search-bar input").toggle(300, "swing");
-//     $(".search-bar button").toggle(300, "swing");
-//   });
+window.addEventListener("DOMContentLoaded", function () {
+  const searchIcon = document.getElementById("search-icon");
+  if (searchIcon) {
+    searchIcon.addEventListener("click", function () {
+      document.getElementById("search-bar").classList.toggle("open");
+    });
+  }
 
-//   //animate to the top of window
-//   $(".to-top").click(function () {
-//     $("html, body").animate({ scrollTop: 0 }, 500);
-//   });
-// });
+  const toTopArrow = document.getElementById("to-top");
+  function isInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  document.addEventListener("scroll", function () {
+    if (isInViewport(toTopArrow)) {
+      toTopArrow.style.opacity = "0.6";
+    } else {
+      toTopArrow.style.opacity = "0";
+    }
+  });
+
+  toTopArrow.addEventListener("click", function () {
+    window.scroll({ top: 0, left: 0, behavior: "smooth" });
+  });
+});
